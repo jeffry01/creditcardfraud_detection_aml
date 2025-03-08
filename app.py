7d32d8c1f1d56e8e73987c327ad82d9bf1c0b9a7
jeffry01 <jeffryjoseph116@gmail.com>
2025-03-08 21:01:06 +0530

    new env

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 import joblib
+import pickle   
 
 app = Flask(__name__, template_folder="Templates/")
 
